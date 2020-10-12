53faffb304796488ae7a9e86693374aefff7015f
nokazn <nokazn@gmail.com>
2020-10-12 14:33:38 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    // switch 文での prettier との競合を防ぐ
+    indent: [2, 2, { SwitchCase: 1 }],
+    'lines-between-class-members': 0,
+    'no-console': 1,
+
+    /**
+     * eslint-plugin-import
+     */
+    'import/extensions': [
+      2,
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        json: 'never',
+      },
+    ],
+    'import/no-unresolved': 0,
+    // typescript-eslint の no-use-before-define を有効にする
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+    // 'import/no-unresolved': [1, { commonjs: true, amd: true }],
+    'import/no-extraneous-dependencies': [1, { devDependencies: true }],
+    'import/prefer-default-export': 0,
+
+    // typescript-eslint の no-unuserd-vars を有効にする
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+  },
+};
---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import * as path from 'path';
+import * as slsw from 'serverless-webpack';
+import nodeExternals from 'webpack-node-externals';
+import * as ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type { Configuration } from 'webpack';
+
+const config: Configuration = {
+  context: __dirname,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+  resolve: {
+    extensions: ['.mjs', '.json', '.ts'],
+    symlinks: false,
+    cacheWithContext: false,
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+          ],
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true,
+      eslintOptions: {
+        cache: true
+      }
+    })
+  ],
+};
+
+export default config;
